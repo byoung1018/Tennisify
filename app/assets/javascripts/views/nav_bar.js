b3186d2f19719fdcb2e946b6daf1872822aea37c
Bryce <byoung1018@gmail.com>
2015-02-12 13:51:37 -0800

    fix create meeting button

--- app/assets/javascripts/views/nav_bar.js
+++ app/assets/javascripts/views/nav_bar.js
@@ -2,20 +2,22 @@ Tennisify.Views.NavBar = Backbone.View.extend({
   initialize: function () {
     var navbar = $(".navbar");
     this.$el.html(navbar.html())
-    debugger
+    navbar.html(this.$el)
   },
   events: {
     "click .create-meeting": "newMeeting",
   },
 
   newMeeting: function (event) {
-    debugger
     event.preventDefault();
-    debugger
     var meeting = new Tennisify.Views.newMeeting({});
     this.showModal(meeting);
-    debugger
     $('#modal').modal('toggle')
   },
 
+  showModal: function (view) {
+    this._modalView && this._modalView.remove();
+    this._modalView = view;
+    $("#modal-view").html(view.render().$el);
+  },
 });
